e27f1a18fe809b29a1cece9218ab630f4f2f5d19
Pedro Henrique Mendes <pedro.lima@clearsale.com.br>
2022-10-31 16:24:19 -0300

    [feat]: arquetetura do projeto

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/test/**/*.[jt]s?(x)'],
+    verbose: true,
+    testTimeout: 30000,
+    reporters: [
+      'default',
+      [
+        'jest-junit',
+        { outputDirectory: 'output/report_xml', outputName: 'report.xml' }
+      ],
+      [
+        'jest-html-reporters',
+        {
+          publicPath: './output',
+          filename: 'index.html',
+          pageTitle: 'Test API publica',
+          logoImgPath: '',
+          expand: false,
+          openReport: false
+        }
+      ]
+    ]
+  };
+  
